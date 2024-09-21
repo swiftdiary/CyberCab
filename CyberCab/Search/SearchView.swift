41b444771367d51c8b6784ded44bd15fcf65fd20
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2024-09-22 03:13:12 +0500

    [Feat] added SearchView TextField

--- CyberCab/Search/SearchView.swift
+++ CyberCab/Search/SearchView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchObservable = SearchObservable()
+    
     var body: some View {
-        Text("Hello, Search!")
+        @Bindable var searchObservable = searchObservable
+        
+        ZStack {
+            
+            Color.csBackground
+                .ignoresSafeArea()
+            
+            VStack {
+                TextField("Search...", text: $searchObservable.searchText)
+                    .padding()
+                    .padding(.leading, 30)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "magnifyingglass")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color.primary)
+                            .padding(.leading)
+                    }
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .padding()
+                
+                ScrollView {
+                    
+                }
+            }
+            .padding()
+        }
+        .task {
+            do {
+                try await searchObservable.getCabs()
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
