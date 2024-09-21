345bb7ab0f952938959e561d0e3cbe2dfb957612
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-09-21 15:31:24 +0500

    Initial Commit

---
+++ CyberCab/CyberCabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyberCabApp.swift
+//  CyberCab
+//
+//  Created by Akbar Khusanbaev on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CyberCabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
