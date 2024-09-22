05dd62351df3b46abb4bc8d68e775df68bb9eacc
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-09-22 08:29:25 +0500

    Session + Checkout Etc....

--- CyberCab/Sessions/SessionCheckoutView.swift
+++ CyberCab/Sessions/SessionCheckoutView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct SessionCheckoutView: View {
+    @Environment(CustomNavigation.self) private var navigation
     let availableHour: AvailableHour
+    @State private var sessionCheckoutObservable = SessionCheckoutObservable()
+    @State private var displayQRCode: Bool = false
     
     var body: some View {
+        @Bindable var sessionCheckoutObservable = sessionCheckoutObservable
         ZStack {
             Color.csBackground.ignoresSafeArea()
             
             VStack {
-                Text("Hello, SessionCheckoutView!")
+                Spacer()
+                Text("Simulation Of Payment.")
+                Text("\(availableHour.price)")
+                Spacer()
+                CC_PrimaryButton("PAY") {
+                    Task {
+                        do {
+                            try await sessionCheckoutObservable.purchase()
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+        }
+        .task {
+            await sessionCheckoutObservable.setAvailableHour(availableHour)
+            do {
+                try await sessionCheckoutObservable.getUser()
+            } catch {
+                print(error)
+            }
+        }
+        .onChange(of: sessionCheckoutObservable.hasPurchasedItem) { oldValue, newValue in
+            if newValue {
+                displayQRCode = true
+            }
+        }
+        .fullScreenCover(isPresented: $displayQRCode) {
+            VStack {
+                if let uiImage = sessionCheckoutObservable.generateQRCode(from: sessionCheckoutObservable.session?.id ?? "https://apple.com") {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                }
+                CC_PrimaryButton("OK") {
+                    navigation.path.removeAll()
+                }
             }
         }
     }
