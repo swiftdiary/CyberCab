edcb438502a809b4893f49ee47c5b493d9172962
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-09-22 06:50:04 +0500

    Booking, Session Creation sections done

---
+++ CyberCab/Sessions/SessionCheckoutView.swift
@@ -0,0 +1,22 @@
+//
+//  SessionCheckoutView.swift
+//  CyberCab
+//
+//  Created by Akbar Khusanbaev on 22/09/24.
+//
+
+import SwiftUI
+
+struct SessionCheckoutView: View {
+    let availableHour: AvailableHour
+    
+    var body: some View {
+        ZStack {
+            Color.csBackground.ignoresSafeArea()
+            
+            VStack {
+                Text("Hello, SessionCheckoutView!")
+            }
+        }
+    }
+}
